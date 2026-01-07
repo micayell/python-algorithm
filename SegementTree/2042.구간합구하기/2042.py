a90cde196d2c6dfd29e5eb2d13e09ebf02e0573d
김창주 <micayell98@naver.com>
2026-01-08 07:57:28 +0900

    segement-tree 문제 추가

---
+++ SegementTree/2042.구간합구하기/2042.py
@@ -0,0 +1,22 @@
+# 수의 개수, 수의 변경이 일어나는 횟수, 구간의 합을 구하는 횟수
+
+# n개의 수가 주어짐
+
+# a,b,c
+# a가 1인 경우 b번째 수를 c로 바꾸고
+# a가 2인 경우 b번째 수부터 c번째 수까지의 합을 구하여 출력
+
+n,m,k = map(int,input().split())
+
+arr=[int(input()) for _ in range(n)]
+
+for _ in range(m+k):
+    res=21e8
+    a,b,c=map(int,input().split())
+    if a == 1:
+        arr[b-1] = c
+    elif a == 2:
+        res = sum(arr[b-1:c])
+
+    if res != 21e8:
+        print(res)
